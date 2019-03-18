89c37d3f0fd6f7cf7cb5338cae303187e9877751
Valery <valera071998@gmail.com>
2019-03-18 15:27:48 +0300

    [Add] tts_client.py. Usage.

---
+++ robotis_op2_tts/tts_client.py
@@ -0,0 +1,43 @@
+if __name__ == '__main__':
+    """
+    Example of using Robotis OP2 Text-to-Speech (TTS) client.
+    
+    1. Create TTS configuration and place it inside ./config;
+    2. Pass path to configuration file and text (string or file) as params to program;
+        You can get support:
+        
+            $ python tts_client.py -h
+            usage: tts_client.py [-h] [-t TEXT] [-f FILE] [-c CONFIG]
+            
+            Robotis OP2 Text-to-Speech (TTS) client. To learn more visit:
+            https://github.com/valera0798/Robotis-OP2-TTS
+            
+            optional arguments:
+              -h, --help            show this help message and exit
+              -t TEXT, --text TEXT  text to synthesise speech.
+              -f FILE, --file FILE  path to text file with content to synthesise speech.
+              -c CONFIG, --config CONFIG
+                                    path to TTS configuration file.
+    
+    3. Create RobotisOP2TTS object;
+    4. Set TTS configuration to object;
+    4. Call synthesise_* method. Passed source of text will be caught automatically.
+    """
+    # add path to current module to PYTHONPATH environment variable
+    import sys
+    sys.path.insert(0, '.')
+
+    from cli.parser import parse_arguments
+
+    dict_args = parse_arguments()
+    dict_config_tts = dict_args["config"]
+
+    tts = RobotisOP2TTSClient(dict_config_tts)
+
+    if dict_args.get("text"):
+        str_text = dict_args["text"]
+        tts.synthesise_speech(str_text)
+    elif dict_args.get("file"):
+        file_text = open(dict_args["file"], 'r')
+        tts.synthesise_speech(file_text)
+        file_text.close()
