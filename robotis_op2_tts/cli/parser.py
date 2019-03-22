0f8bab00e52da014a54b626e9a9da25fc72348ba
Valery <valera071998@gmail.com>
2019-03-22 12:15:55 +0300

    [Change] CLI. Parser interface implementation.

--- robotis_op2_tts/cli/parser.py
+++ robotis_op2_tts/cli/parser.py
@@ -14,7 +14,16 @@ def validate_configuration_file_path(args):
     :param args: parsed arguments.
     :return: None (args can be modified).
     """
-    pass
+    if not args.config:
+        print("There is no passed path to configuration file. Default one will be used.")
+        args.config = os.path.abspath("./config/default.json")
+
+    print("Configuration file path = {}".format(args.config))
+    if args.config:
+        if os.path.isfile(args.config):
+            print("Configuration file was found.")
+        else:
+            raise ConfigurationFileNotFoundException()
 
 
 def validate_text_source(args):
@@ -28,7 +37,20 @@ def validate_text_source(args):
     :param args: parsed arguments.
     :return: None (args can be modified).
     """
-    pass
+    if not args.text and not args.file:
+        raise NoSourceTextException()
+
+    if args.text and args.file:
+        raise SeveralSourceTextsException()
+    elif args.file:
+        print("Text file path = {}".format(args.file))
+        if os.path.isfile(args.file):
+            print("Text file was found.")
+        else:
+            raise SourceTextFileNotFoundException()
+    elif args.text:
+        pass
+    print("Text to speech was provided.")
 
 
 def parse_arguments():
@@ -46,4 +68,19 @@ def parse_arguments():
 
     :return: dict - parsed arguments.
     """
-    pass
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Robotis OP2 Text-to-Speech (TTS) client. "
+                                                 "To learn more visit: https://github.com/valera0798/Robotis-OP2-TTS")
+    parser.add_argument('-t', '--text', type=str, help="text to synthesise speech.")
+    parser.add_argument('-f', '--file', type=str, help="path to text file with content to synthesise speech.")
+    parser.add_argument('-c', '--config', type=str, help="path to TTS configuration file.")
+    args = parser.parse_args()
+
+    try:
+        validate_configuration_file_path(args)
+        validate_text_source(args)
+    except RobotisOP2TTSException as e:
+        sys.exit(str(e))
+
+    return vars(args)
