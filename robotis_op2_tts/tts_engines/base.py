36375081323ce925cc5e357222ef7a244776b64f
Valery <valera071998@gmail.com>
2019-03-28 21:49:26 +0300

    [Add] TTS client delegate abstract class.

--- robotis_op2_tts/tts_engines/base.py
+++ robotis_op2_tts/tts_engines/base.py
@@ -41,3 +41,39 @@ class AbstractTTSClient(InterfaceTTSClient):
                 makedirs(self._str_path_output_dir)
             except FileExistsError:
                 pass
+
+
+class AbstractTTSClientDelegate(InterfaceTTSClient):
+    """
+    Abstract TTS client delegate class.
+        - Declares abstract structure of TTS client delegates.
+        - Behaves like TTS client.
+        - Should be used as parent of all TTS client delegates.
+    """
+    _config_tts = None              # configuration of specific TTS cloud_clients client
+    _client_tts = None              # specific TTS cloud_clients client
+    _str_command_play_audio = None  # command to call audio player
+
+    def __init__(self, dict_config):
+        """
+        Constructs instance of AbstractTTSClientDelegate class.
+
+        :param dict_config: configuration of TTS client.
+        """
+        super().__init__()
+        self.set_configuration(dict_config)
+
+    def set_configuration(self, dict_config):
+        """
+        Sets configuration of delegate instance.
+            - Sets input configuration as _config_tts field.
+
+        * Validates passed configuration before set.
+
+        :param dict_config: dict - configuration of TTS client.
+        :return: None (fields will be initialized).
+        """
+        if self.validate_configuration(dict_config):
+            self._str_command_play_audio = dict_config['audio_file_player']['command']
+            dict_config.pop('audio_file_player')    # to not to duplicate data
+            self._config_tts = dict_config
