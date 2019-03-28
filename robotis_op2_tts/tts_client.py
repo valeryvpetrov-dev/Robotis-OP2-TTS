b57749c68f1e6690f10f81bb850136611b4be380
Valery <valera071998@gmail.com>
2019-03-28 21:04:02 +0300

    [Change] RobotisOP2TTSClient. set_configuration implementation.

--- robotis_op2_tts/tts_client.py
+++ robotis_op2_tts/tts_client.py
@@ -1,96 +1,77 @@
-class RobotisOP2TTSClient:
+from base import InterfaceTTSClient
+
+
+class RobotisOP2TTSClient(InterfaceTTSClient):
     """
     Robotis OP 2 Text-to-Speech (TTS) client class.
+        - Behaves like InterfaceTTSClient.
     """
-    def __init__(self, str_path_config_file):
+    _config_tts = None              # general configuration of Robotis OP2 TTS.
+    _client_tts_cloud = None        # TTS cloud client
+    _client_tts_onboard = None      # TTS onboard client
+
+    def __init__(self, str_path_file_config):
         """
         Constructor of TTS client object.
 
         1. Initializes object;
-        2. Sets passed configuration.
+        2. Processes passed configuration.
+        3. Interacts .
         """
         super().__init__()
-        self.set_configuration(str_path_config_file)
+        self.set_configuration(str_path_file_config)
 
-    def set_configuration(self, str_path_config_file):
+    def set_configuration(self, str_path_file_config):
         """
-        Setter for config_tts field.
+        Sets configuration of RobotisOP2TTSClient.
 
         * Configuration file will be parsed to dictionary.
-        * Configuration dictionary will be validated before set.
+        * Configuration dictionary will be validated superficially before set.
+        * Corresponding TTS clients will be created as fields of RobotisOP2Client instance.
+            - Actually, it is mediator to specific TTS client.
 
-        :param str_path_config_file: path to tts configuration file.
-        :return: None (object field __config_tts will be set).
+        :param str_path_file_config: path to TTS configuration file.
+        :return: None (object field _config_tts will be set).
         """
         from config.parser import parse_configuration
+        from tts_engines.cloud.tts_delegate import TTSCloudClientDelegate
+        from tts_engines.onboard.tts_delegate import TTSOnboardClientDelegate
 
-        dict_config_tts = parse_configuration(str_path_config_file)
-        self.__config_tts = dict_config_tts
+        dict_config_tts = parse_configuration(str_path_file_config)
+        if self.validate_configuration(dict_config_tts):
+            self._config_tts = dict_config_tts
+
+            dict_engines_tts = self._config_tts['tts_engines']
+            dict_config_cloud_tts = dict_engines_tts['cloud'].copy()
+            dict_config_onboard_tts = dict_engines_tts['onboard'].copy()
+
+            if dict_config_cloud_tts:
+                dict_config_cloud_tts.pop('priority', None) # information about priority is not valuable for TTS client
+                dict_config_cloud_tts['audio_file_format'] = self._config_tts['audio_file_format']
+                dict_config_cloud_tts['audio_file_player'] = self._config_tts['audio_file_player']
+                self._client_tts_cloud = TTSCloudClientDelegate(dict_config_cloud_tts)
+            elif dict_config_onboard_tts:
+                dict_config_onboard_tts.pop('priority', None) # information about priority is not valuable for TTS client
+                dict_config_onboard_tts['audio_file_format'] = self._config_tts['audio_file_format']
+                dict_config_onboard_tts['audio_file_player'] = self._config_tts['audio_file_player']
+                self._client_tts_onboard = TTSOnboardClientDelegate(dict_config_onboard_tts)
 
     def synthesise_audio(self, source_text):
         """
-        Creates audio file with passed source_text spoken.
-
-        * Audio file format - .mp3.
-        * File will be stored in ./audio directory.
-
-        :param source_text: string or file with text for synthesise.
-        :return: audio file with synthesised speech.
+        Implements corresponding method of interface parent class.
         """
-        pass    # TODO
+        pass
 
     def synthesise_speech(self, source_text):
         """
-        Speaks passed source_text.
-
-        * Actually calls synthesise_audio and passes result to audio player.
-
-        :param source_text: string or file with text for synthesise.
-        :return: None.
+        Implements corresponding method of interface parent class.
         """
-        pass    # TODO
+        pass
 
+    def validate_configuration(self, dict_config):
+        """
+        Implements corresponding method of interface parent class.
 
-if __name__ == '__main__':
-    """
-    Example of using Robotis OP2 Text-to-Speech (TTS) client.
-    
-    1. Create TTS configuration and place it inside ./config;
-    2. Pass path to configuration file and text (string or file) as params to program;
-        You can get support:
-        
-            $ python tts_client.py -h
-            usage: tts_client.py [-h] [-t TEXT] [-f FILE] [-c CONFIG]
-            
-            Robotis OP2 Text-to-Speech (TTS) client. To learn more visit:
-            https://github.com/valera0798/Robotis-OP2-TTS
-            
-            optional arguments:
-              -h, --help            show this help message and exit
-              -t TEXT, --text TEXT  text to synthesise speech.
-              -f FILE, --file FILE  path to text file with content to synthesise speech.
-              -c CONFIG, --config CONFIG
-                                    path to TTS configuration file.
-    
-    3. Create RobotisOP2TTS object;
-    4. Set TTS configuration to object;
-    4. Call synthesise_* method. Passed source of text will be caught automatically.
-    """
-    # add path to current module to PYTHONPATH environment variable
-    import sys
-    sys.path.insert(0, '.')
-
-    from cli.parser import parse_arguments
-
-    dict_args = parse_arguments()
-    str_path_config_file = dict_args["config"]
-
-    tts = RobotisOP2TTSClient(str_path_config_file)
-
-    if dict_args.get("text"):
-        str_text = dict_args["text"]
-        tts.synthesise_speech(str_text)
-    elif dict_args.get("file"):
-        file_text = open(dict_args["file"], 'r')
-        tts.synthesise_speech(file_text)
-        file_text.close()
+        Validates configuration superficially. TTS clients details will not be touched.
+        """
+        pass
