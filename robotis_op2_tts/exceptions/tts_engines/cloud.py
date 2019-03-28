b86c6a2dfe8ba1eae44aee64c342059acf4b81f9
Valery <valera071998@gmail.com>
2019-03-28 22:00:14 +0300

    [Add] TTS cloud client exceptions.

--- robotis_op2_tts/exceptions/tts_engines/cloud.py
+++ robotis_op2_tts/exceptions/tts_engines/cloud.py
@@ -7,3 +7,35 @@ class TTSCloudClientException(RobotisOP2TTSException):
     """
     def __init__(self, message) -> None:
         super().__init__(message)
+
+
+class CallParamNotFoundException(TTSCloudClientException):
+    """
+    Call param not found exception class.
+    """
+    def __init__(self) -> None:
+        super().__init__("Cloud TTS call param not found exception.")
+
+
+class NetworkParamNotFoundException(TTSCloudClientException):
+    """
+    Network param not found exception class.
+    """
+    def __init__(self) -> None:
+        super().__init__("Cloud TTS network param not found exception.")
+
+
+class NetworkNotAccessibleException(TTSCloudClientException):
+    """
+    Network is not accessible exception class.
+    """
+    def __init__(self) -> None:
+        super().__init__("Network is not accessible, so cloud_clients TTS can't work.")
+
+
+class NetworkSpeedNotApplicableException(TTSCloudClientException):
+    """
+    Network speed is not applicable exception class.
+    """
+    def __init__(self) -> None:
+        super().__init__("Network speed is not applicable, so cloud_clients TTS can't work properly.")
