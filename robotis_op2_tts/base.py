fa1bc96181967580ae0d0d70ed4e77b9a60398f1
Valery <valera071998@gmail.com>
2019-04-01 11:41:02 +0300

    [Add] LoggableInterface declaration.

--- robotis_op2_tts/base.py
+++ robotis_op2_tts/base.py
@@ -37,3 +37,35 @@ class InterfaceTTSClient:
         :return: bool - indicator of succeeded validation.
         """
         pass
+
+
+class LoggableInterface:
+    """
+    Abstract class that is responsible for logging.
+        - Declares abstract structure of loggable class.
+        - Should be used as parent of all classes that needs in logging facilities.
+    """
+    logger = None                   # logger instance that provides logging interface
+    _console_handler = None         # console logs handler
+    _console_formatter = None       # console logs formatter
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        import logging
+        from sys import stdout
+
+        # changing default logging level of root logger from WARNING to DEBUG
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+
+        # create specific logger for heir
+        self.logger = logging.getLogger(self.__class__.__name__)
+        # adjust handler to redirect logs to stdout
+        self.console_handler = logging.StreamHandler(stdout)
+        self.console_handler.setLevel(logging.DEBUG)
+        # adjust LogRecord format
+        self.console_formatter = logging.Formatter("%(asctime)s - %(process)d:%(name)s - %(levelname)s:%(message)s")
+        self.console_handler.setFormatter(self.console_formatter)
+        # bind handler with logger
+        self.logger.addHandler(self.console_handler)
