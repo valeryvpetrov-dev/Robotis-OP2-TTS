7b49aae3f4063d1551e951291fa4b14cf005168d
Valery <valera071998@gmail.com>
2019-04-07 10:47:15 +0300

    [Change] Say shortcut. Robotis OP2 specification.

--- shortcuts/say.sh
+++ shortcuts/say.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # adjust variables for virtualenvwrapper usage
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.4
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 # source virtualenvwrapper functions to current script
 source `which virtualenvwrapper.sh`
 
 # switch to TTS virtual environment
 # ! replace name with your own
-workon robotis-op2-tts-venv
+workon TTS-venv
 
 # call python tts.py play -t TEXT
 python tts.py play -t "$1"
