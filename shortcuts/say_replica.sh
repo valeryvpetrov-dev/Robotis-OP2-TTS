039001102a04d127926f12e15a69ff8ae56ccbd2
Valery <valera071998@gmail.com>
2019-04-25 13:19:20 +0300

    [Add] Shortcuts. Saying replicas.

---
+++ shortcuts/say_replica.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# source local ~/.bashrc
+source ~/.bashrc
+
+VIRTUALENV_NAME="Robotis-OP2-TTS-venv"
+ROSCORE_NAME="roscore"
+
+call_replica () {   # calls replica
+# pre configuration
+    roslaunch robotis_op_onboard_launch robotis_op_whole_robot.launch
+    # source virtualenvwrapper commands to current script
+    source $(which virtualenvwrapper.sh)
+    # switch to TTS virtual environment
+    workon ${VIRTUALENV_NAME}
+
+# action
+    # start motion
+    rosrun robotis_dance robotis_dance "$1.txt"
+    # start speaking
+    python tts.py play -f "./kids_lessons/$1.ssml" > /dev/null  # silent
+
+# post configuration
+    deactivate
+}
+
+# check if replica name is valid
+regexp_replica_name="L[0-9]+\.replica[0-9]+(_[0-9]+)?"
+if [[ $1 =~ $regexp_replica_name ]]
+then
+    echo "Replica name '$1' matches pattern."
+else
+    echo "Replica name '$1' does not match pattern."
+    exit 128
+fi
+
+# check if roscore is running
+if pgrep -x ${ROSCORE_NAME} > /dev/null
+then
+    echo "'$ROSCORE_NAME' is already running."
+else
+    echo "'$ROSCORE_NAME' is not running. Now it is starting."
+    roscore
+fi
+
+call_replica $1
