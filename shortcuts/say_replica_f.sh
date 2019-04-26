275353edeae8af1243a39c3f58dccfb45fa01af3
Valery <valera071998@gmail.com>
2019-04-26 11:25:08 +0300

    [Add] Shortcuts. Force and safe scripts.

---
+++ shortcuts/say_replica_f.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# source local ~/.bashrc
+source ~/.bashrc
+
+# source virtualenvwrapper commands to current script
+source $(which virtualenvwrapper.sh)
+# switch to TTS virtual environment
+workon "Robotis-OP2-TTS-venv"
+
+# action
+# start motion
+rosrun robotis_dance robotis_dance "$1.txt" &   # run on background
+# start speaking
+python tts.py play -f "./kids_lessons/$1.ssml" > /dev/null &    # run on background, silent
+
+# post configuration
+deactivate
