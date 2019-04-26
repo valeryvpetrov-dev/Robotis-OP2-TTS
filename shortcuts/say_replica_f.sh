57640497ca385546e8d55712b88b7c9fa8812c3a
Valery <valera071998@gmail.com>
2019-04-26 14:36:14 +0300

    [Fix] Shortcuts. Force say replica. Manual switch to python venv.

--- shortcuts/say_replica_f.sh
+++ shortcuts/say_replica_f.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
-
-# source virtualenvwrapper commands to current script
-source $(which virtualenvwrapper.sh)
-# switch to TTS virtual environment
-workon "Robotis-OP2-TTS-venv"
-
 # action
 # start motion
 rosrun robotis_dance robotis_dance "$1.txt" > /dev/null &   # run on background
 # start speaking
 python tts.py play -f "./kids_lessons/$1.ssml" > /dev/null &    # run on background, silent
-
-# post configuration
-deactivate
